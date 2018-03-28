55453b8f3fb230350d43e7b0e6be945df29a8f2c
ramavtar <ramavtarj9@gmail.com>
2018-03-28 08:28:08 +0530

    first

---
+++ data-structure/src/org/linkedlist/NthNode.java
@@ -0,0 +1,20 @@
+package org.linkedlist;
+
+public class NthNode {
+	class Node {
+		Node next;
+		int data;
+		
+		Node(int data){
+			this.data=data;
+		}
+
+	}
+	
+	Node head;
+	public static void main(String[] args) {
+		
+		
+	}
+
+}
