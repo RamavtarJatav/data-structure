5dc703ca3466a7a73bdee9a53bbd78b4769c85d9
ramavtar <ramavtarj9@gmail.com>
2018-04-20 08:23:22 +0530

    111

--- first/src/main/java/sort/QuickSortOnLinkedList.java
+++ first/src/main/java/sort/QuickSortOnLinkedList.java
@@ -54,27 +54,33 @@ public class QuickSortOnLinkedList {
 			return;
 		Node node = head;
 		Node pi = prePartitionNode(head, null);
-		//System.out.println(pi.data);
-		//System.out.print();
-		quickSort(node,pi);
+		// System.out.println(pi.data);
+		// System.out.print();
+		quickSort(head, pi);
+		quickSort1(pi);
+
+	}
 	
+	static void quickSort1(Node pi) {
+		Node preprei = pi;
 		
-	}
-	
+ 	}
+
 	void quickSort(Node start, Node pi) {
-		//Node pid = prePartitionNode(start, pi);
-	
+		
+		Node pid = prePartitionNode(start, pi);
+		
+		print(head);
+
 	}
-	
-	
 
 	Node prePartitionNode(Node node, Node pi) {
 		if (node == null)
 			return null;
 
 		Node pivot = node;
-		Node prelow = node;
-		Node preprelow = null;
+		Node prei = node;
+		Node preprei = null;
 
 		while (node.next != pi) {
 			print();
@@ -82,20 +88,20 @@ public class QuickSortOnLinkedList {
 			System.out.println();
 
 			if (pivot.data > node.next.data) {
-				Node first = prelow.next;
+				Node first = prei.next;
 				Node second = node.next;
 
 				if (first == second) {
 					// doing nothing;
 				} else if (first.next == second) {
-					prelow.next = second;
+					prei.next = second;
 					Node temp = second.next;
 					second.next = first;
 					first.next = temp;
 					node = second;
 
 				} else {
-					prelow.next = second;
+					prei.next = second;
 					Node firstAft = first.next;
 					Node secondAft = second.next;
 					second.next = firstAft;
@@ -103,32 +109,34 @@ public class QuickSortOnLinkedList {
 					first.next = secondAft;
 
 				}
-				preprelow = prelow;
-				prelow = prelow.next;
+				preprei = prei;
+				prei = prei.next;
 			}
 
 			node = node.next;
 		}
-		
-		//print();
-		
-		Node prelowaft = prelow.next;	
-		prelow.next = pivot.next;
-		preprelow.next = pivot;
-		pivot.next = prelowaft;
-		head = prelow;
-		
-		if(pi != null) {
-			node.next = pi;
+
+		print();
+        //if nodes are adjecet
+		if (preprei == pivot) {
+			
+			pivot.next = prei.next;
+			prei.next = pivot;
+
+		} else {
+        // nodes are not adjecnt
+			Node preiaft = prei.next;
+			prei.next = pivot.next;
+			preprei.next = pivot;
+			pivot.next = preiaft;
+
+			
 		}
 		
-//		Node afterprelow = prelow.next;   
-//	 	prelow.next = pivot.next;
-//	 	preprelow.next = pivot;
-//	 	pivot.next = afterprelow ;
-//	 	head = prelow;
-	 	
-
+		if (pi != null) {
+			node.next = pi;
+		}
+		head = prei;
 
 		return pivot;
 	}
@@ -137,15 +145,15 @@ public class QuickSortOnLinkedList {
 
 		QuickSortOnLinkedList qks = new QuickSortOnLinkedList();
 		// 5 6 10 8 2 3 1 0
-		 qks.add(9);
-		 qks.add(18);
-		 qks.add(3);
+		qks.add(9);
+		qks.add(18);
+		qks.add(3);
 		qks.add(6);
 		qks.add(2);
 		qks.add(10);
 		qks.add(4);
 		qks.add(8);
-		 qks.print();
+		qks.print();
 		qks.quickSort();
 		qks.print();
 
