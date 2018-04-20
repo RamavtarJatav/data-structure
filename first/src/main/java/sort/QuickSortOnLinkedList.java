b89981103536af8baffa3c6a916726978c0335d8
ramavtar <ramavtar.jatav@qolsys.com>
2018-04-20 12:36:52 +0530

    111

--- first/src/main/java/sort/QuickSortOnLinkedList.java
+++ first/src/main/java/sort/QuickSortOnLinkedList.java
@@ -53,38 +53,36 @@ public class QuickSortOnLinkedList {
 		if (head == null)
 			return;
 		Node node = head;
-		Node pi = prePartitionNode(head, null);
+		Node pi = partitionNode(head, null, null);
 		// System.out.println(pi.data);
 		// System.out.print();
-		quickSort(head, pi);
-		quickSort1(pi);
-
-	}
-	
-	static void quickSort1(Node pi) {
-		Node preprei = pi;
-		
- 	}
-
-	void quickSort(Node start, Node pi) {
-		
-		Node pid = prePartitionNode(start, pi);
-		
-		print(head);
+		if (head.next != pi || head != pi)
+			quickSort(head, pi, null);
+		if (pi.next != null || pi.next.next != null)
+			quickSort(pi.next, pi, null);
 
 	}
 
-	Node prePartitionNode(Node node, Node pi) {
-		if (node == null)
-			return null;
+	void quickSort(Node start, Node pi, Node end) {
 
-		Node pivot = node;
-		Node prei = node;
-		Node preprei = null;
+		Node pid = partitionNode(start, pi, end);
+		if (start.next != pid || start != pi)
+			quickSort(start, pid, pi);
+		if (pid.next != null || pid.next.next != null)
+			quickSort(pid.next, pid, pi);
 
-		while (node.next != pi) {
+	}
+
+	Node partitionNode1(Node preStart, Node start, Node aftEnd) {
+		Node pivot = start;
+		Node prei = start;
+		Node node = start;
+		Node preprei = preStart;
+		while (node != aftEnd) {
+
 			print();
-			System.out.print("pre iteration=" + node.data + " iteranation node=" + node.next.data);
+			System.out.print("pre iteration=" + node.data);
+			System.out.print(" iteranation node=" + node.next.data);
 			System.out.println();
 
 			if (pivot.data > node.next.data) {
@@ -115,30 +113,134 @@ public class QuickSortOnLinkedList {
 
 			node = node.next;
 		}
-
-		print();
-        //if nodes are adjecet
-		if (preprei == pivot) {
-			
-			pivot.next = prei.next;
-			prei.next = pivot;
-
-		} else {
-        // nodes are not adjecnt
-			Node preiaft = prei.next;
-			prei.next = pivot.next;
-			preprei.next = pivot;
-			pivot.next = preiaft;
-
-			
-		}
 		
-		if (pi != null) {
-			node.next = pi;
+		if(preStart == null) {
+			if (prei == pivot) {
+				return pivot;
+			}
 		}
-		head = prei;
+		return preprei;
+	}
 
-		return pivot;
+	Node partitionNode(Node node, Node pi, Node end) {
+		if (node == null)
+			return null;
+
+		Node pivot = node;
+		Node prei = node;
+		Node preprei = null;
+		if (pi == null || pi.next != node) {
+			while (node.next != pi) {
+				print();
+				System.out.print("pre iteration=" + node.data);
+				System.out.print(" iteranation node=" + node.next.data);
+				System.out.println();
+
+				if (pivot.data > node.next.data) {
+					Node first = prei.next;
+					Node second = node.next;
+
+					if (first == second) {
+						// doing nothing;
+					} else if (first.next == second) {
+						prei.next = second;
+						Node temp = second.next;
+						second.next = first;
+						first.next = temp;
+						node = second;
+
+					} else {
+						prei.next = second;
+						Node firstAft = first.next;
+						Node secondAft = second.next;
+						second.next = firstAft;
+						node.next = first;
+						first.next = secondAft;
+
+					}
+					preprei = prei;
+					prei = prei.next;
+				}
+
+				node = node.next;
+			}
+
+			print();
+			// if nodes are adjecet
+			if (prei == pivot) {
+				return pivot;
+			}
+			if (preprei == pivot) {
+
+				pivot.next = prei.next;
+				prei.next = pivot;
+
+			} else {
+				// nodes are not adjecnt
+				Node preiaft = prei.next;
+				prei.next = pivot.next;
+				preprei.next = pivot;
+				pivot.next = preiaft;
+
+			}
+
+			head = prei;
+			return pivot;
+		} else {
+			preprei = pi;
+			while (node.next != null) {
+				if (pivot.data > node.next.data) {
+					Node first = prei.next;
+					Node second = node.next;
+
+					if (first == second) {
+						// doing nothing;
+					} else if (first.next == second) {
+						prei.next = second;
+						Node temp = second.next;
+						second.next = first;
+						first.next = temp;
+						node = second;
+
+					} else {
+						prei.next = second;
+						Node firstAft = first.next;
+						Node secondAft = second.next;
+						second.next = firstAft;
+						node.next = first;
+						first.next = secondAft;
+
+					}
+					preprei = preprei.next;
+					prei = prei.next;
+				}
+
+				node = node.next;
+			}
+
+			if (prei == pivot) {
+				return pivot;
+			}
+
+			if (preprei == pivot) {
+
+				pivot.next = prei.next;
+				prei.next = pivot;
+
+			} else {
+				// nodes are not adjecnt
+				Node preiaft = prei.next;
+				prei.next = pivot.next;
+				preprei.next = pivot;
+				pivot.next = preiaft;
+
+			}
+
+			pi.next = prei;
+			return pivot;
+
+		}
+
 	}
 
 	public static void main(String[] args) {
