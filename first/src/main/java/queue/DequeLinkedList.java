e352b331c021037617d7d1583938ba249fba32ce
ramavtar <ramavtar.jatav@qolsys.com>
2018-06-26 10:55:45 +0530

    new code

--- first/src/main/java/queue/DequeLinkedList.java
+++ first/src/main/java/queue/DequeLinkedList.java
@@ -12,54 +12,98 @@ public class DequeLinkedList {
 	}
 
 	Node front, rear;
-	int size;
-
-	void addFront(int data) {
-
-	}
 
 	void addRear(int data) {
-		if(rear == null) {
+		if (rear == null) {
 			rear = new Node(data);
 			front = rear;
-		}else {
+		} else {
 			Node temp = new Node(data);
 			temp.next = rear;
-			temp.prev = front;
+			rear.prev = temp;
 			rear = temp;
 		}
-			
+	}
+
+	void removeFront() {
+         if(front == null)
+        	return;
+         front = front.prev;
+         front.next = null;
+	}
+
+	void addFront(int data) {
+		if (front == null) {
+			front = new Node(data);
+			rear = front;
+		} else {
+			Node temp = new Node(data);
+			temp.prev = front;
+			front.next = temp;
+			front = temp;
+		}
 
 	}
 
-	void removeFront(int data) {
-
-	}
-
-	void removeRear(int data) {
-
-	}
-	
-	void printFromFront() {
-		if(front == null)
+	void removeRear() {
+		if(rear == null) 
 			return;
-		printFromFront(front);
+		rear = rear.next;
+		rear.prev = null;
+
 	}
-	
+
+	void printFromFront() {
+		if (front == null)
+			return;
+
+		printFromFront(front);
+		System.out.println();
+
+	}
+
 	void printFromFront(Node node) {
-		System.out.print(node.data);
-		if(node.next != null ) {
-			printFromFront(node.next);
-			
+		System.out.print(node.data + " ");
+		if (node.prev != null) {
+			printFromFront(node.prev);
+		}
+	}
+
+	void printFromRear() {
+		if (rear == null)
+			return;
+		printFromRear(rear);
+		System.out.println();
+	}
+
+	void printFromRear(Node node) {
+		System.out.print(node.data + " ");
+		if (node.next != null) {
+			printFromRear(node.next);
 		}
 	}
 
 	public static void main(String[] args) {
-		DequeLinkedList dLL = new DequeLinkedList();
-		for(int i = 0 ; i < 10 ; i++) {
-			dLL.addRear(i);
-		}
-		dLL.printFromFront();
+		DequeLinkedList deque = new DequeLinkedList();
+		for (int i = 0; i <= 10; i++)
+			deque.addRear(i);
+
+		for (int i = 11; i <= 20; i++)
+			deque.addFront(i);
+
+		deque.printFromFront();
+		deque.removeFront();
+		deque.printFromFront();
+		deque.removeFront();
+		deque.printFromFront();
+		deque.removeRear();
+		deque.printFromFront();
+		deque.removeRear();
+		deque.printFromFront();
+		
+		
+		
+
 	}
 
 }
